9a9170d8a33e9010d0f81c89f68f84c2b566e058
Aleksaxe <36526654+Aleksaxe@users.noreply.github.com>
2018-08-06 23:02:22 +0700

    1

---
+++ CDEKtest/src/sample/DBWorker.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBWorker {
+
+    public DBWorker(){
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        ConData conData=new ConData();
+        try (Connection connection =
+                     DriverManager.getConnection(conData.getURL(),conData.getUSERNAME(),conData.getPASSWORD());
+             Statement statement=connection.createStatement()
+        ){
+            if (!connection.isClosed()) System.out.println("success");
+            statement.execute("insert into users (name, age, email) values ('Tes', 12, 't12@yandex.ru');");
+            // statement.executeUpdate("UPDATE users set name='valenok' where id=2");
+            //statement.execute("select * from users");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
